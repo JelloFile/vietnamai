f952f2dfbf49f828037a2e892d0df3c22fbf6822
JelloFile <123781425+JelloFile@users.noreply.github.com>
2024-05-01 20:51:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -353,7 +353,8 @@ function s4c17_1(){
   opt4.style.display = "none";
   document.getElementById('ai').style.display="none"
   document.getElementById('ohno').style.display="";
-  
+  var audio = new Audio('/mathcompass/themesong.mp3');
+audio.play();
 }
 function sarcasm(){
   var randomize = Math.floor(Math.random() * 10)+1
